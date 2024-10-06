252648c165796a249c6c3473703189def46b7772
Kavin Prasath S <141269905+stark-kavin@users.noreply.github.com>
2024-10-07 05:23:22 +0530

    user-select

--- millan-detail/script.js
+++
@@ -1,9 +0,0 @@
-var pass = document.getElementById("show-pass-checkbox");
-pass.addEventListener("click",function(){
-    if(pass.checked){
-        document.getElementById("pass").type = "text"
-    }else{
-        document.getElementById("pass").type = "password"
-    }
-});
-
