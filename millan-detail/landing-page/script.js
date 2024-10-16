3dbb538d6fa2d348d9e8739bd51fb396af03b9de
Kavin Prasath S <141269905+stark-kavin@users.noreply.github.com>
2024-10-16 21:28:05 +0000

    Completed Landing page

--- millan-detail/landing-page/script.js
+++ millan-detail/landing-page/script.js
@@ -5,4 +5,3 @@ document.querySelector("#find-detailer").addEventListener("click",function() {
 document.querySelector("#close-popup-1").addEventListener("click",function() {
     document.getElementById("popup-1").dataset.show = "false";
 })
-
